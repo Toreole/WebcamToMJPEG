70e800dbdf2284be287ab2267dd2e9308c0954e7
Toreole <toreole.official@gmail.com>
2022-09-05 22:17:59 +0200

    Printing name of first device found

--- VirtualCamStream/VirtualCamStream/VirtualCamStream.cpp
+++ VirtualCamStream/VirtualCamStream/VirtualCamStream.cpp
@@ -3,7 +3,55 @@
 
 #include <iostream>
 
+#include <mfapi.h>
+#include <mfcaptureengine.h>
+#include <mfidl.h>
+#pragma comment(lib, "Mfplat.lib")
+#pragma comment(lib, "Mf.lib")
+
+#include <atlstr.h>
+
 int main()
 {
+    HRESULT hr;
+    IMFAttributes* pMFAttributes = NULL;
+    UINT32 count = 0;
+
+    IMFActivate** ppDevices = NULL;
+
+    //create an attribute to hold search criteria
+    hr = MFCreateAttributes(&pMFAttributes, 1);
+
+    //Request information on the video capture devices present.
+    if (SUCCEEDED(hr))
+    {
+        hr = pMFAttributes->SetGUID(
+            MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
+            MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID
+        );
+    }
+
+    //enumerate the devices
+    if (SUCCEEDED(hr))
+    {
+        hr = MFEnumDeviceSources(pMFAttributes, &ppDevices, &count);
+    }
+
+    LPWSTR str;
+    UINT l;
+
+    if (SUCCEEDED(hr))
+    {
+        ppDevices[0]->GetAllocatedString(MF_DEVSOURCE_ATTRIBUTE_FRIENDLY_NAME, &str, &l);
+        for (UINT i = 0; i < l; ++i)
+        {
+            char c = str[i];
+            std::cout << c;
+        }
+    }
+
+
+    std::cout << '\n';
+
     std::cout << "Hello World!\n";
 }
